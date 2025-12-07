62e7ad461efaac11ff3befd9dd238649ecea7f1e
DaoChaShao <27882272@qq.com>
2025-12-07 15:59:58 +0800

    feat: update predictor to use LSTM model and enhance prediction logic with OpenAI integration

--- predictor.py
+++ predictor.py
@@ -8,71 +8,93 @@
 
 from pathlib import Path
 from random import randint
-from re import match
-from torch import Tensor, load, device, no_grad, nn
-from tqdm import tqdm
+from re import search
+from torch import Tensor, load, device, no_grad, nn, tensor, long, argmax
 
 from src.configs.cfg_rnn import CONFIG4RNN
-from src.nets.rnn4classification import NormalRNNForClassification
-from src.utils.helper import Timer
+from src.configs.cfg_types import Language, Tokens, LSTMTask
+from src.nets.lstm4classification import LSTMRNNForClassification
+from src.utils.apis import OpenAITextCompleter
+from src.utils.helper import Timer, read_yaml
+from src.utils.highlighter import red, green, yellow, blue, purple
+from src.utils.nlp import spacy_batch_tokeniser
 from src.utils.PT import item2tensor
 from src.utils.SQL import SQLiteIII
 from src.utils.stats import create_full_data_split, load_json
-from src.utils.THU import cut_only
+
+
+def pad_seq2tensor(sequences: list[list[int]], max_len: int, PAD: int = 0) -> Tensor:
+    """ Convert sequences to PyTorch Tensor
+    :param sequences: list of word2id sequences
+    :param max_len: maximum sequence length
+    :param PAD: padding index
+    :return: PyTorch Tensor of sequences
+    """
+    padded: list[list[int]] = []
+
+    for seq in sequences:
+        if len(seq) > max_len:
+            new = seq[:max_len]
+        else:
+            new = seq + [PAD] * (max_len - len(seq))
+        padded.append(new)
+
+    return tensor(padded, dtype=long)
 
 
 def main() -> None:
     """ Main Function """
     # Get the data from the database: Method II
-    with SQLiteIII(CONFIG4RNN.DATABASE.TABLE, CONFIG4RNN.DATABASE.COL) as db:
-        data = db.get_all_data()
-        print(len(data))
-        print()
+    # Get the data from the database
+    table: str = "news"
+    cols: dict[str, type[int | str]] = {"label": int, "content": str}
+    with SQLiteIII(table, cols, CONFIG4RNN.FILEPATHS.SQLITE) as db:
+        database = db.fetch_all([col for col in cols.keys()])
+        # print(len(database))
+        # print()
+    # pprint(data[:3])
+    # print(len(data))
+
+    MAX_SEQ_LEN: int = 48
 
     with Timer("Next Word Prediction"):
         # Separate the data
-        _, _, sentences = create_full_data_split(data)
+        _, _, data = create_full_data_split(database)
 
         # Tokenise the data
         # amount: int | None = 100
         amount: int | None = None
-        items: list[str] = []
+        contents: list[str] = [content for _, content in data]
         if amount is None:
-            for line in tqdm(sentences, total=len(sentences), desc="Tokenizing Test Data"):
-                for item in cut_only(line):
-                    if item in CONFIG4RNN.PUNCTUATIONS.CN or match(r"^[\u4e00-\u9fff]+$", item):
-                        items.append(item)
+            news: list[list[str]] = spacy_batch_tokeniser(
+                contents, lang=Language.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
+            )
         else:
-            for line in tqdm(sentences[:amount], total=amount, desc="Tokenizing Test Data"):
-                for item in cut_only(line):
-                    if item in CONFIG4RNN.PUNCTUATIONS.CN or match(r"^[\u4e00-\u9fff]+$", item):
-                        items.append(item)
-        # print(items)
-        # print()
+            news: list[list[str]] = spacy_batch_tokeniser(
+                contents[:amount], lang=Language.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
+            )
+        # print(news)
 
         # Load the dictionary and convert
         dic: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY)
         dictionary: dict[str, int] = load_json(dic)
         reversed_dictionary: dict = {idx: word for word, idx in dictionary.items()}
 
-        # Convert the whole sentence to sequence using dictionary
-        UNK_IDX: int = dictionary["<UNK>"]
-        sequence: list[int] = [dictionary.get(item, UNK_IDX) for item in items]
-        # print(sequence)
-        # print()
-
-        # Pick up a random sequence token
-        idx: int = randint(0, len(sequence) - 1 - CONFIG4RNN.PREPROCESSOR.MAX_SEQUENCE_LEN)
-        seq_token: list[int] = sequence[idx: idx + CONFIG4RNN.PREPROCESSOR.MAX_SEQUENCE_LEN]
-        # print(seq_token)
-        print()
+        # Convert a random sentence to sequence using dictionary
+        idx: int = randint(0, len(news) - 1)
+        sequence: list[str] = news[idx]
+        seq: list[int] = [dictionary.get(item, Tokens.UNK) for item in sequence]
+        # print(seq)
 
         # Convert the token to a tensor
-        X: Tensor = item2tensor(seq_token, embed=True, accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR)
+        X: Tensor = item2tensor(seq, embedding=True, accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR)
         # Add batch size
         X = X.unsqueeze(0)
         # print(X)
-        # print()
+        # Get the relevant label
+        labels: list[int] = [label for label, _ in data]
+        y: Tensor = item2tensor(labels[idx], accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR)
+        print(y)
 
         # Load the save model parameters
         params: Path = Path(CONFIG4RNN.FILEPATHS.SAVED_NET)
@@ -80,14 +102,15 @@ def main() -> None:
             print(f"Model {params.name} Exists!")
 
             # Set up a model and load saved parameters
-            model = NormalRNNForClassification(
+            model = LSTMRNNForClassification(
                 len(dictionary),
                 embedding_dim=CONFIG4RNN.PARAMETERS.EMBEDDING_DIM,
                 hidden_size=CONFIG4RNN.PARAMETERS.HIDDEN_SIZE,
                 num_layers=CONFIG4RNN.PARAMETERS.LAYERS,
-                num_classes=len(dictionary),
+                num_classes=CONFIG4RNN.PARAMETERS.CLASSES,
                 dropout_rate=CONFIG4RNN.PREPROCESSOR.DROPOUT_RATIO,
                 accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR,
+                task=LSTMTask.CLASSIFICATION
             )
             dict_state: dict = load(params, map_location=device(CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR))
             model.load_state_dict(dict_state)
@@ -95,34 +118,48 @@ def main() -> None:
             print("Model Loaded Successfully!")
 
             # Prediction
-            TOP_K: int = 5
             with no_grad():
-                predictions = model(X)
-                # print(predictions.shape, predictions)
-                # print()
+                logist: Tensor = model(X)
+                # print(logist)
+                probabilities: Tensor = nn.functional.softmax(logist)
+                # print(probabilities)
+                y_pred: Tensor = argmax(probabilities, dim=1)
+                print(y_pred)
 
-                # Set a temperature scaler controlling categorical randomness, lower values, lower randomness
-                temperature: float = CONFIG4RNN.PARAMETERS.TEMPERATURE
-                if 0.5 <= temperature <= 1.5:
-                    predictions = predictions / temperature
-                else:
-                    raise ValueError("Temperature should be between 0.5 and 1.5.")
+                correct: bool = (y_pred.item() == y)
+                # print(correct)
+                print(blue("Positive") if y == 2 else purple("Negative") if y == 1 else yellow("Neutral"))
 
-                # Convert the output to probabilities
-                probs: Tensor = nn.functional.softmax(predictions, dim=-1)
-                # print(probs.shape, probs)
-                # print()
+                rate: str = green("Bingo") if correct == True else red("Damn")
+                print(f"The content of the selected news {idx}:\n{contents[idx]}")
+                print(f"The prediction of the news {idx} is {rate} !")
 
-                # Get top k value from probabilities (values, indices)
-                _, indices = probs.topk(TOP_K, dim=-1, sorted=True)
-                # print(indices)  # size: (batches, top_k)
-                # print()
+            # Prompt Engineering with OpenAI API
+            key: Path = Path(CONFIG4RNN.FILEPATHS.API_KEY)
+            config: dict = read_yaml(key)
+            API_KEY: str = config["openai"]["api_key"]
+            opener = OpenAITextCompleter(API_KEY, temperature=0)
+            role: str = "You are a professional financial expert with cross-cultural expertise."
+            rating: str = """
+                        0: Neutral Review
+                        1: Negative Review
+                        2: Positive Review
+                        """
+            prompt: str = f"""
+                        Give a brief explanation in Chinese after reading the following review:
+                        {contents[idx]}.
+                        Please analyze it, give a reason, and provide a rating (Only return number) as follows:
+                        {rating}.
+                        """
+            explanation = opener.client(role, prompt)
+            print(explanation)
 
-                # Get the relevant words
-                print("".join(items[idx: idx + CONFIG4RNN.PREPROCESSOR.MAX_SEQUENCE_LEN]))
-                pred_words: list[str] = [reversed_dictionary.get(idx) for idx in indices.squeeze().tolist()]
-                print(pred_words)
-                print()
+            match = search(r"\b[0-2]\b", explanation)
+            pred_label: int | None = int(match.group()) if match else None
+            print(f"Predicted Label from OpenAI: {pred_label}")
+
+            result = green("Bingo") if (pred_label == y_pred) else red("Damn")
+            print(f"The prediction between OpenAI and RNN Model: {result} !")
         else:
             print(f"Model {params.name} does not exist!")
 
