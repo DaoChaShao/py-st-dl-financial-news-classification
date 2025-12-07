c61dab155590e6e582f3c0ea52ba08a34f3223b5
DaoChaShao <27882272@qq.com>
2025-12-07 16:44:12 +0800

    feat: refactor evaluator to use LSTM model and improve data processing

--- evaluator.py
+++ evaluator.py
@@ -7,78 +7,76 @@
 # @Desc     :
 
 from pathlib import Path
-from re import match
 from random import randint
-from torch import Tensor, load, device, no_grad, nn
+from torch import Tensor, load, device, no_grad, nn, argmax
 from tqdm import tqdm
 
 from src.configs.cfg_rnn import CONFIG4RNN
-from src.configs.cfg_types import SeqTask
+from src.configs.cfg_types import Language, Tokens, LSTMTask
 from src.dataloaders.dataloader4torch import TorchDataLoader
-from src.datasets.seq_next_step import TorchDataset4SeqPredictionNextStep
-from src.nets.rnn4classification import NormalRNNForClassification
+from src.datasets.seq_classification import TorchDataset4Seq2Classification
+from src.nets.lstm4classification import LSTMRNNForClassification
 from src.utils.helper import Timer
 from src.utils.highlighter import starts, lines, red, green
+from src.utils.nlp import spacy_batch_tokeniser, build_word2id_seqs
 from src.utils.SQL import SQLiteIII
 from src.utils.stats import create_full_data_split, load_json
-from src.utils.THU import cut_only
 
 
 def main() -> None:
     """ Main Function """
     # Get the data from the database: Method II
-    with SQLiteIII(CONFIG4RNN.DATABASE.TABLE, CONFIG4RNN.DATABASE.COL) as db:
-        data = db.get_all_data()
-        # print(len(data))
+    table: str = "news"
+    cols: dict[str, type[int | str]] = {"label": int, "content": str}
+    with SQLiteIII(table, cols, CONFIG4RNN.FILEPATHS.SQLITE) as db:
+        database = db.fetch_all([col for col in cols.keys()])
+        # print(len(database))
         # print()
+    # pprint(data[:3])
+    # print(len(data))
 
     with Timer("Next Word Prediction"):
         # Separate the data
-        _, _, sentences = create_full_data_split(data)
+        _, _, data = create_full_data_split(database)
 
-        # Set a dictionary
+        # Tokenise the data
         # amount: int | None = 100
         amount: int | None = None
-        items: list[str] = []
+        contents: list[str] = [content for _, content in data]
         if amount is None:
-            for line in tqdm(sentences, total=len(sentences), desc="Tokenizing Train Data"):
-                for item in cut_only(line):
-                    if item in CONFIG4RNN.PUNCTUATIONS.CN or match(r"^[\u4e00-\u9fff]+$", item):
-                        items.append(item)
+            news: list[list[str]] = spacy_batch_tokeniser(
+                contents, lang=Language.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
+            )
         else:
-            for line in tqdm(sentences[:amount], total=amount, desc="Tokenizing Train Data"):
-                for item in cut_only(line):
-                    if item in CONFIG4RNN.PUNCTUATIONS.CN or match(r"^[\u4e00-\u9fff]+$", item):
-                        items.append(item)
-        # print(items)
-        # print()
+            news: list[list[str]] = spacy_batch_tokeniser(
+                contents[:amount], lang=Language.EN, batches=CONFIG4RNN.PREPROCESSOR.BATCHES
+            )
+        # print(news)
 
         # Load the dictionary and convert
         dic: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY)
         dictionary: dict[str, int] = load_json(dic)
-        reversed_dictionary: dict = {idx: word for word, idx in dictionary.items()}
 
-        # Convert the whole sentence to sequence using dictionary
-        UNK_IDX: int = dictionary["<UNK>"]
-        sequence: list[int] = [dictionary.get(item, UNK_IDX) for item in items]
-        # print(sequence)
-        # print()
+        # Convert sentences to sequence using dictionary
+
+        sequences: list[list[int]] = build_word2id_seqs(news, dictionary, Tokens.UNK)
+
+        # Get the relevant label
+        labels: list[int] = [label for label, _ in data]
 
         # Set up dataset
-        dataset = TorchDataset4SeqPredictionNextStep(
-            sequence,
-            seq_max_len=CONFIG4RNN.PREPROCESSOR.MAX_SEQUENCE_LEN,
-            mode=SeqTask.SEQ2ONE,
-            pad_token=dictionary["<PAD>"]
+        MAX_SEQ_LEN: int = 48
+        dataset = TorchDataset4Seq2Classification(
+            sequences, labels,
+            seq_max_len=MAX_SEQ_LEN,
+            pad_token=dictionary[Tokens.PAD]
         )
-        starts()
-        print("Data Preprocessing Summary:")
-        lines()
-        print(f"Test dataset: {len(dataset)} Samples")
-        print(f"Dictionary Size: {len(dictionary)}")
-        print(f"Reversed Dictionary Size: {len(reversed_dictionary)}")
-        starts()
-        print()
+        # starts()
+        # print("Data Preprocessing Summary:")
+        # lines()
+        # print(f"Test dataset: {len(dataset)} Samples")
+        # print(f"Dictionary Size: {len(dictionary)}")
+        # starts()
         # Set up dataloader
         dataloader = TorchDataLoader(
             dataset,
@@ -88,7 +86,6 @@ def main() -> None:
         )
         idx: int = randint(0, len(dataloader) - 1)
         # print(dataloader[idx])
-        # print()
 
         # Load the save model parameters
         params: Path = Path(CONFIG4RNN.FILEPATHS.SAVED_NET)
@@ -96,14 +93,15 @@ def main() -> None:
             print(f"Model {params.name} Exists!")
 
             # Set up a model and load saved parameters
-            model = NormalRNNForClassification(
+            model = LSTMRNNForClassification(
                 len(dictionary),
                 embedding_dim=CONFIG4RNN.PARAMETERS.EMBEDDING_DIM,
                 hidden_size=CONFIG4RNN.PARAMETERS.HIDDEN_SIZE,
                 num_layers=CONFIG4RNN.PARAMETERS.LAYERS,
-                num_classes=len(dictionary),
+                num_classes=CONFIG4RNN.PARAMETERS.CLASSES,
                 dropout_rate=CONFIG4RNN.PREPROCESSOR.DROPOUT_RATIO,
                 accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR,
+                task=LSTMTask.CLASSIFICATION
             )
             dict_state: dict = load(params, map_location=device(CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR))
             model.load_state_dict(dict_state)
@@ -111,50 +109,42 @@ def main() -> None:
             print("Model Loaded Successfully!")
 
             # Predict
-            TOP_K: int = 5
-            total: int = 0
-            correct_counter: int = 0
+            total = 0
+            correct_counter = 0
+
             with no_grad():
-                for X, y in dataloader:
+                for X, y in tqdm(dataloader, total=len(dataloader), desc="Evaluating Trained Model:"):
                     logits = model(X)
-                    # Convert the output to probabilities
-                    probs: Tensor = nn.functional.softmax(logits, dim=-1)
-                    # print(probs.shape, probs)
-                    # print()
+                    # Convert the output to probabilities along the class dimension
+                    probabilities: Tensor = nn.functional.softmax(logits, dim=1)
+                    # Get predicted labels
+                    y_pred: Tensor = argmax(probabilities, dim=1)
 
-                    # Get top k value from probabilities (values, indices)
-                    _, indices = probs.topk(TOP_K, dim=-1, sorted=True)
-                    # print(indices)  # size: (batches, top_k)
-                    # print()
+                    # Compare with true labels
+                    correct: Tensor = (y_pred == y)
 
-                    # Add y batch size and compare the predictions and targets
-                    comparison = (y.unsqueeze(1) == indices).any(dim=-1)
-                    # print(comparison)
-                    # print(comparison.sum().item())
-
+                    # Update counters
                     total += y.size(0)
-                    correct_counter += comparison.sum().item()
+                    correct_counter += correct.sum().item()
 
-                # Get trained model evaluation result
-                starts()
-                print("Trained Model Evaluation")
-                lines()
-                print(f"Total Samples: {total}")
-                print(f"Positive Samples: {green(correct_counter)}")
-                print(f"Negative Samples: {red(total - correct_counter)}")
-                print(f"Top-{TOP_K} Accuracy: {correct_counter / total:.2%}")
-                starts()
-                print()
-                """
-                ****************************************************************
-                Trained Model Evaluation
-                ----------------------------------------------------------------
-                Total Samples: 995682
-                Positive Samples:       264744
-                Negative Samples:       730938
-                Top-5 Accuracy: 26.59%
-                ****************************************************************
-                """
+            # Accuracy
+            accuracy = correct_counter / total
+            starts()
+            print("Trained Model Evaluation")
+            lines()
+            print(f"Total Samples: {total}")
+            print(f"Correct Predictions: {correct_counter}")
+            print(f"Accuracy: {accuracy:.2%}")
+            starts()
+            """
+            ****************************************************************
+            Trained Model Evaluation
+            ----------------------------------------------------------------
+            Total Samples: 9448
+            Correct Predictions: 9440
+            Accuracy: 99.92%
+            ****************************************************************
+            """
         else:
             print(f"Model {params.name} does not exist!")
 
